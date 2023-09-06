e9d009150330b0ef058a2bcd00f2f395d182a460
adina-m <adina_popescu23@yahoo.com>
2023-09-06 15:03:41 +0300

    [CA-6-rabbitmq] - queues for save and update student

--- src/main/java/com/catalog/apimodel/StudentDTO.java
+++ src/main/java/com/catalog/apimodel/StudentDTO.java
@@ -1,9 +1,15 @@
 package com.catalog.apimodel;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
 
 @Data
-public class StudentDTO {
+@AllArgsConstructor
+@NoArgsConstructor
+public class StudentDTO implements Serializable {
     private Integer id;
     private String studentName;
     private String studentClass;
